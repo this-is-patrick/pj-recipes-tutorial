b42fbf0ed0818bb61fc1b70d1a19503319fceabf
Patrick Jimison <patrick.j.jimison@intel.com>
2023-02-20 11:02:20 -0600

    initial setup and test environment

---
+++ src/pages/company/history.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const history = () => {
+  return <div>this is the history of our company!</div>;
+};
+
+export default history;
---
+++ src/pages/company/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const index = () => {
+  return <div>this is the company page</div>;
+};
+
+export default index;
