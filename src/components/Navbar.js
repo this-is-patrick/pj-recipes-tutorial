64d41265f9b3ce7c0bedba1df24b61c307e5a5d2
Patrick Jimison <patrick.j.jimison@intel.com>
2023-02-27 09:03:04 -0600

    Added links and react-icons to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,38 @@
 import React from "react";
 import { Link } from "gatsby";
-
+import { FaAlignJustify } from "react-icons/fa";
+import logo from "../assets/images/logo.svg";
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">home</Link>
-        </li>
-        <li>
-          <Link to="/about">about</Link>
-        </li>
-        <li>
-          <Link to="/contact">contact</Link>
-        </li>
-        <li>
-          <Link to="/tags">tags</Link>
-        </li>
-        <li>
-          <Link to="/recipes">recipes</Link>
-        </li>
-      </ul>
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="simply recipes" />
+          </Link>
+          <button className="nav-btn">
+            <FaAlignJustify />
+          </button>
+        </div>
+        <div className="nav-links show-links"></div>
+        <Link to="/" className="nav-link" activeClassName="active-link">
+          home
+        </Link>
+        <Link to="/recipes" className="nav-link" activeClassName="active-link">
+          recipes
+        </Link>
+        <Link to="/tags" className="nav-link" activeClassName="active-link">
+          tags
+        </Link>
+        <Link to="/about" className="nav-link" activeClassName="active-link">
+          about
+        </Link>
+        <div className="nav-link contact-link">
+          <Link to="/contact" className="btn">
+            contact
+          </Link>
+        </div>
+      </div>
     </nav>
   );
 };
