75352335a86398877f9836b9962ec2368075b4e0
Patrick Jimison <patrick.j.jimison@intel.com>
2023-03-07 08:51:17 -0600

    Added main text and image to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,27 @@
 import * as React from "react";
 import Layout from "../components/Layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 export default function Home() {
   return (
     <Layout>
-      <h1>Home page</h1>
+      <main className="page">
+        <header className="hero">
+          <StaticImage
+            src="../assets/images/main.jpeg"
+            alt="Flower and Eggs"
+            className="hero-img"
+            placeholder="tracedSVG"
+            layout="fullWidth"
+          />
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>simply recipes</h1>
+              <h4>no fluff, just recipes</h4>
+            </div>
+          </div>
+        </header>
+      </main>
     </Layout>
   );
 }
