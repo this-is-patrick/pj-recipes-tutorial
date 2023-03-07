7dd6f94f9428fc476d8ef422014fc170a5710c8c
Patrick Jimison <patrick.j.jimison@intel.com>
2023-03-07 08:28:19 -0600

    Set up about page text and image

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,35 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { StaticImage } from "gatsby-plugin-image";
+import { Link } from "gatsby";
 
 const About = () => {
   return (
     <Layout>
-      <h1>About page</h1>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>I'm baby coloring book poke taxidermy</h2>
+            <p>
+              Taxidermy forage glossier letterpress heirloom before they sold
+              out you probably haven't heard of them banh mi biodiesel chia.
+            </p>
+            <p>
+              Taiyaki tumblr flexitarian jean shorts brunch, aesthetic salvia
+              retro.
+            </p>
+            <Link to="/contact" className="btn">
+              contact
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="Person Pouring Salt in Bowl"
+            className="about-img"
+            placeholder="blurred"
+          />
+        </section>
+      </main>
     </Layout>
   );
 };
