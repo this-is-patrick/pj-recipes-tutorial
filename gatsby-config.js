2a1fe20df86f8abdbfbfc62a1bbe44e170c71329
Patrick Jimison <patrick.j.jimison@intel.com>
2023-03-07 09:07:38 -0600

    Add SiteMetadata for graphql sandbox testing

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,17 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  siteMetadata: {
+    title: "Simply Recipes",
+    description: "Nice and clean recipes site",
+    author: "@pjjimiso",
+    person: { name: "patrick", age: 32 },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      { name: "patrick", age: 32 },
+      { name: "domenica", age: 25 },
+    ],
+  },
   plugins: [
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
