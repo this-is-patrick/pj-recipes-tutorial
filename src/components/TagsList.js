4a8916ad9128271dae4e7d8807f9af8b21abe771
Patrick Jimison <patrick.j.jimison@intel.com>
2023-06-05 08:45:21 -0700

    Populate tags page, add tags list to home page, added setupTags function to retrieve tags array

--- src/components/TagsList.js
+++ src/components/TagsList.js
@@ -1,9 +1,22 @@
 import React from "react";
+import setupTags from "../utils/setupTags";
+import { Link } from "gatsby";
 
-const TagsList = () => {
+const TagsList = ({ recipes }) => {
+  const newTags = setupTags(recipes);
   return (
-    <div>
-      <h4>tags list</h4>
+    <div className="tag-container">
+      <h4>recipes</h4>
+      <div className="tags-list">
+        {newTags.map((tag, index) => {
+          const [text, value] = tag;
+          return (
+            <Link to={`/${text}`} key={index}>
+              {text} ({value})
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,12 +1,39 @@
 import React from "react";
 import Layout from "../components/Layout";
-
-const Tags = () => {
+import { graphql, Link } from "gatsby";
+import setupTags from "../utils/setupTags";
+const Tags = ({ data }) => {
+  const newTags = setupTags(data.allContentfulBlog.nodes);
+  console.log(newTags);
   return (
     <Layout>
-      <h1>Tags page</h1>
+      <main className="page">
+        <section className="tags-page">
+          {newTags.map((tag, index) => {
+            const [text, value] = tag;
+            return (
+              <Link to={`/${text}`} key={index} className="tag">
+                <h5>{text}</h5>
+                <p>{value}</p>
+              </Link>
+            );
+          })}
+        </section>
+      </main>
     </Layout>
   );
 };
+
+export const query = graphql`
+  query {
+    allContentfulBlog {
+      nodes {
+        content {
+          tags
+        }
+      }
+    }
+  }
+`;
 
 export default Tags;
